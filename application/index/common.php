69bb06138d858f1598e280dc3b21f79023e94df6
zhangdayu <zhangdayu@iprun.com>
2020-07-01 15:53:53 +0800

    添加人员单位下拉菜单

--- application/index/common.php
+++ application/index/common.php
@@ -1,2 +1,11 @@
 <?php
-
+function recursion($result,$parentid=0){
+    static $list=array();
+    foreach ($result as $k => $v){
+        if($v['pid']==$parentid){
+            $list[]=$v;
+            recursion($result,$v['id']);
+        }
+    }
+    return $list;
+}
--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -40,7 +40,8 @@ class User extends Controller{
         }
         $res=\app\index\model\User::where(['id'=>$id])->find();
         if ($res){
-            return json(['code'=>200,'data'=>$res]);
+            $parent=$this->getparent($res['team_id']);
+            return json(['code'=>200,'data'=>$res,'parent'=>$parent]);
         }else{
             return json(['code'=>420,'msg'=>'获取数据错误']);
         }
@@ -76,5 +77,49 @@ class User extends Controller{
         }else{
             return json(['code'=>420,'msg'=>'删除错误']);
         }
+    }
+    //工作单位下拉菜单
+    public function getTeam(){
+        $id=input('get.id',0);
+        if($id==0){
+            $res=\app\index\model\Team::where("pid=0 && is_team=0")->order('sort desc,id asc')->field('id,name')->select();
+        }else{
+            $res=\app\index\model\Team::where("pid=".$id." && is_team=0")->order('sort desc,id asc')->field('id,name')->select();
+        }
+        $data=[];
+        foreach ($res as $key=>$val){
+            $val['count']=\app\index\model\Team::where(['pid'=>$val['id']])->count();
+            $data[0]=[
+                'id'=>0,
+                'name'=>'请选择',
+                'count'=>0
+            ];
+            $data[$key+1]=$val;
+        }
+        return json(['code'=>200,'content'=>$data]);
+    }
+
+    //获取team_id父级 子级
+    private function getparent($id){
+        if ($id==0){
+            return '';
+        }
+        $res=\app\index\model\Team::where(['id'=>$id])->field('id,name,pid,level')->find();
+        $res_p=\app\index\model\Team::where(['id'=>$res['pid']])->field('id,name,pid,level')->find();
+        if ($res['level']==2){
+            return $res_p['name']." > ".$res['name'];
+//            return ['team_id_1'=>$res_p['name'],'team_id_2'=>$res['name'],'team_id_3'=>''];
+        }
+        if ($res['level']==3){
+            $res_pp=\app\index\model\Team::where(['id'=>$res_p['pid']])->field('id,name,pid')->find();
+            return $res_pp['name']." > ".$res_p['name']." > ".$res['name'];
+//            return ['team_id_1'=>$res_pp['name'],'team_id_2'=>$res_p['name'],'team_id_3'=>$res['name']];
+        }
+    }
+
+    public function getTest(){
+        $res=\app\index\model\Team::order('sort desc')->select();
+        $r=recursion($res,0);
+        return json($r);
     }
 }
