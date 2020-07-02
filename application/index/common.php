e3a238b9821024c41c0962a5c4682b48b8e17eba
zhangdayu <zhangdayu@iprun.com>
2020-07-02 17:46:19 +0800

    添加cxselect

--- application/index/common.php
+++ application/index/common.php
@@ -1,10 +1,15 @@
 <?php
 function recursion($result,$parentid=0){
-    static $list=array();
+    $list=array();
     foreach ($result as $k => $v){
         if($v['pid']==$parentid){
+//            $list[$k]['n']=$v['name'];
+            $v['n']=$v['name'];
+            $v['s']=recursion($result,$v['id']);
+//            if ($v['level']!=1){
+//                $list[]['s']['n']=recursion($result,$v['id']);
+//            }
             $list[]=$v;
-            recursion($result,$v['id']);
         }
     }
     return $list;
