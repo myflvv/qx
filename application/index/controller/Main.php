d6c23da3c8679426db98b6f922529a0a5651dd28
nothingness <dayu.my@163.com>
2020-07-22 23:37:58 +0800

    添加推荐

--- application/index/controller/Main.php
+++ application/index/controller/Main.php
@@ -1,10 +1,16 @@
 <?php
 namespace app\index\controller;
 
+use app\index\model\PicModel;
 use think\Controller;
 class Main extends Controller{
 
     public function getIndex(){
         return $this->fetch('index');
+    }
+
+
+    public function getPicList(){
+
     }
 }
---
+++ application/index/model/PicModel.php
@@ -0,0 +1,6 @@
+<?php
+namespace app\index\model;
+use think\Model;
+class PicModel extends Model{
+    protected $table="qx_pic";
+}
